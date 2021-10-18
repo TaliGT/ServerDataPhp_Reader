daf02337680c126d9375d8accfbf86e18fce0f18
TaliGT <90297090+TaliGT@users.noreply.github.com>
2021-10-18 01:41:23 -0700

    Add files via upload

---
+++ ServerDataPhp_Reader/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerDataPhp_Reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string lel = "";
+            Random a = new Random();
+            int b = a.Next(1, 3);
+            if (b == 1)
+            {
+                lel = "growtopia1.com";
+            }
+            else if (b == 2)
+            {
+                lel = "growtopia2.com";
+            }
+            else
+            {
+                lel = "Unknown";
+            }
+            string ip = textBox1.Text;
+            var url = "http://" + ip + "/growtopia/server_data.php";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Headers["Accept"] = "*/*";
+            httpRequest.Headers["Host"] = lel;
+            httpRequest.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+            httpRequest.Headers["content-length"] = "38";
+            httpRequest.Headers["user-agent"] = "";
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                MessageBox.Show(result.ToString());
+            }
+
+        }
+    }
+}
